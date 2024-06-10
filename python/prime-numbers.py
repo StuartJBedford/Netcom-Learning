5954d572cfa66b40605f9d98081bad0dc3ae5c2e
StuartJBedford <bigmanbedda@hotmail.com>
2024-06-10 16:35:27 +0100

    added prime task

--- python/prime-numbers.py
+++ python/prime-numbers.py
@@ -1,5 +1,7 @@
+with open("results.txt", 'w') as file:
 
-
-for num in range(2,250):
-    if all(num%i!=0 for i in range(2,num)):
-       print (num)
+    for num in range(2,250):
+        if all(num%i!=0 for i in range(2,num)):
+            print(num)
+            num = str(num)   
+            file.write(f"{num}, ")
